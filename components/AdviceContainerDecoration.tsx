d90f5a82d092fd8bc3ca20d5d5caebabcd379b34
BuzzKill Master <BuzzKillMaster@proton.me>
2023-01-23 12:47:10 +0100

    escape quotation mark

--- components/AdviceContainerDecoration.tsx
+++ components/AdviceContainerDecoration.tsx
@@ -2,7 +2,7 @@ export default function AdviceContainerDecoration() {
     return (
         <div className={"flex items-center rotate-180 mb-4"}>
             <div className={"h-1 -mt-9 bg-slate-600 w-full"}></div>
-            <p className={"text-7xl mx-4"}>"</p>
+            <p className={"text-7xl mx-4"}>&quot;</p>
             <div className={"h-1 -mt-9 bg-slate-600 w-full"}></div>
         </div>
     )
