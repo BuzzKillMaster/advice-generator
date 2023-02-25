fc15f18cbab59782afba7a36c4dab3084e42ec91
BuzzKill Master <BuzzKillMaster@proton.me>
2023-02-25 12:05:28 +0100

    switch advice container to use the advice type

--- components/AdviceContainer.tsx
+++ components/AdviceContainer.tsx
@@ -3,23 +3,29 @@ import AdviceHeading from "./AdviceHeading";
 import AdviceQuotation from "./AdviceQuotation";
 import AdviceContainerDecoration from "./AdviceContainerDecoration";
 import RequestAdviceButton from "./RequestAdviceButton";
+import advice from "../pages/types/advice";
 
 
 export default function AdviceContainer(props: {
     adviceID: number,
     adviceText: string
 }) {
-    const [adviceID, setAdviceID] = useState(props.adviceID)
-    const [adviceText, setAdviceText] = useState(props.adviceText)
+    const [advice, setAdvice] = useState<advice>({
+        id: props.adviceID,
+        text: props.adviceText
+    })
 
     try {
-        window.history.replaceState(null, "", "?id=" + adviceID)
+        window.history.replaceState(null, "", "?id=" + advice.id)
     } catch (e) {}
 
     const updateAdvice = async (attempt: number = 0) => {
         if (attempt >= 3) {
-            setAdviceID(500)
-            setAdviceText("Unfortunately, the server seems to have encountered an error.")
+            setAdvice({
+                id: 500,
+                text: "Unfortunately, the server seems to have encountered an error."
+            })
+
             return
         }
 
@@ -27,13 +33,15 @@ export default function AdviceContainer(props: {
             const response = await fetch("https://api.adviceslip.com/advice", { cache: "no-store" })
             const data = await response.json()
 
-            if (data["slip"]["id"] === adviceID) {
+            if (data["slip"]["id"] === advice.id) {
                 await updateAdvice()
                 return
             }
 
-            setAdviceID(data["slip"]["id"])
-            setAdviceText(data["slip"]["advice"])
+            setAdvice({
+                id: data["slip"]["id"],
+                text: data["slip"]["advice"],
+            })
         } catch (e) {
             await updateAdvice(attempt + 1)
         }
@@ -41,10 +49,10 @@ export default function AdviceContainer(props: {
 
     return (
         <div className={"bg-slate-700 shadow rounded-xl p-10 pb-6 text-white max-w-full w-[40rem] text-center"}>
-            <AdviceHeading adviceID={adviceID}/>
-            <AdviceQuotation quote={adviceText}/>
+            <AdviceHeading adviceID={advice.id}/>
+            <AdviceQuotation quote={advice.text}/>
             <AdviceContainerDecoration/>
-            <RequestAdviceButton handleClick={() => updateAdvice()}/>
+            <RequestAdviceButton handleClick={updateAdvice}/>
         </div>
     )
 }
